98797119cc7167cece8a29f4b9d9ad079ab5a547
Sangamithran <sangamithran97@gmail.com>
2026-03-19 00:50:41 +0530

    fixed a typo

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegsiterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatters=[
+urlpatterns=[
     path('register/', RegsiterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='obtain_pair_view'),
     path('refresh/', TokenRefreshView.as_view(), name='refresh'),
