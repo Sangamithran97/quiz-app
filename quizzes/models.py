76163a24562e16a4d4994fe587f4fa098dcdb0d5
Sangamithran <sangamithran97@gmail.com>
2026-03-19 14:07:23 +0530

    Added Quiz and Question models

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+User= settings.AUTH_USER_MODEL
+
+class Quiz(models.Model):
+    topic= models.CharField(max_length=255)
+    difficulty= models.CharField(max_length=50)
+    question_count= models.IntegerField()
+    created_by= models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.topic} ({self.difficulty})"
+    
+class Question(models.Model):
+    quiz=models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
+    question_text= models.TextField()
+    options= models.JSONField()
+    correct_answer= models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question_text
+    
